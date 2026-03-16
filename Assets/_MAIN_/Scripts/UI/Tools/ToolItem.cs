4f9cb3307a19970bae4119d7cd4fc49f538c7061
temp-Priyansh-Athena <athena.priyansh@gmail.com>
2026-03-16 17:16:10 +0530

    Added Tool System

--- Assets/_MAIN_/Scripts/UI/Tools/ToolItem.cs
+++ Assets/_MAIN_/Scripts/UI/Tools/ToolItem.cs
@@ -1,16 +1,58 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToolItem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Data")]
+    [SerializeField] string toolName;
+    [SerializeField] string toolInfo;
+    [SerializeField] string howToUse;
+    [SerializeField] Sprite icon;
+
+    [Header("References")]
+    [SerializeField] TMP_Text nameTxt;
+    [SerializeField] Image iconImg;
+
+    private void Awake()
     {
-        
+        Name = toolName;
+        Icon = icon;
+        Information = toolInfo;
+        HowToUse = howToUse;
     }
 
-    // Update is called once per frame
-    void Update()
+    public string Name
     {
-        
+        get { return toolName; }
+        set
+        {
+            toolName = value;
+            if (nameTxt != null)
+                nameTxt.text = value;
+        }
+    }
+
+    public string Information
+    {
+        get { return toolInfo; }
+        set { toolInfo = value; }
+    }
+
+    public string HowToUse
+    {
+        get { return howToUse; }
+        set { howToUse = value; }
+    }
+
+    public Sprite Icon
+    {
+        get { return icon; }
+        set
+        {
+            icon = value;
+            if (iconImg != null)
+                iconImg.sprite = value;
+        }
     }
 }
--- Assets/_MAIN_/Scripts/UI/Tools/Tools.cs
+++ Assets/_MAIN_/Scripts/UI/Tools/Tools.cs
@@ -1,16 +1,137 @@
+using DG.Tweening;
+using System;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Tools : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("References")]
+    [SerializeField] List<ToolItem> toolItems;
+    [SerializeField] CanvasGroup toolInfoCg, allToolsCg, toolsPanel;
+
+    [Space(10)]
+    [SerializeField] Image toolIcon;
+    [SerializeField] Button selectBtn;
+    [SerializeField] CanvasGroup unselectBtnCg, openToolBtnCg;
+    [SerializeField] TMP_Text toolTitle, toolInfo, toolUsage, selectBtnTxt;
+
+    [SerializeField] ToolItem selectedToolItemUI; 
+
+    [Space(10), Header("Data")]
+    [TextArea(2, 10), SerializeField] string defaultToolName;
+    [SerializeField] Sprite defaultToolSprite;
+
+    void OnToolSelect(ToolItem _toolItem)
     {
-        
+        if(_toolItem == null)
+        {
+            selectedToolItemUI.Name = defaultToolName;
+            selectedToolItemUI.Icon = defaultToolSprite;
+
+            unselectBtnCg.blocksRaycasts = false;
+            unselectBtnCg.DOFade(0f, 0.5f);
+            return;
+        }
+
+        unselectBtnCg.blocksRaycasts = true;
+        unselectBtnCg.DOFade(1f, 0.5f);
+
+        selectedToolItemUI.Name = _toolItem.Name;
+        selectedToolItemUI.Icon = _toolItem.Icon;
+        selectedToolItemUI.Information = _toolItem.Information;
+        selectedToolItemUI.HowToUse = _toolItem.HowToUse;
     }
 
-    // Update is called once per frame
-    void Update()
+    public UnityEvent<ToolItem> OnToolSelected;
+
+    ToolItem selectedTool;
+    int currIdx = -1;
+
+    public ToolItem GetSelectedTool
     {
-        
+        get { return selectedTool; }
+    }
+
+    public void OpenToolInfo(int index)
+    {
+        if (index < 0 || index >= toolItems.Count) return;
+
+        currIdx = index;
+        ToolItem tool = toolItems[index];
+
+        // Always update UI
+        toolTitle.text = tool.Name;
+        toolInfo.text = tool.Information;
+        toolUsage.text = tool.HowToUse;
+        toolIcon.sprite = tool.Icon;
+
+        // Update button state
+        if (selectedTool != null && selectedTool == tool)
+        {
+            selectBtn.interactable = false;
+            selectBtnTxt.text = "Selected";
+        }
+        else
+        {
+            selectBtn.interactable = true;
+            selectBtnTxt.text = "Select";
+        }
+
+        allToolsCg.DOFade(0f, 0.5f).OnComplete(() =>
+        {
+            allToolsCg.blocksRaycasts = false;
+            toolInfoCg.blocksRaycasts = true;
+            toolInfoCg.DOFade(1f, 0.5f);
+        });
+    }
+
+    public void OpenAllTools()
+    {
+        toolInfoCg.DOFade(0f, 0.5f).OnComplete(() =>
+        {
+            toolInfoCg.blocksRaycasts = false;
+            allToolsCg.blocksRaycasts = true;
+            allToolsCg.DOFade(1f, 0.5f);
+        });
+    }
+
+    public void OpenToolsPanel()
+    {
+        openToolBtnCg.DOFade(0f, 0.35f).OnComplete(() =>
+        {
+            toolsPanel.blocksRaycasts = true;
+            toolsPanel.DOFade(1f, 0.5f);
+            OpenAllTools();
+        });
+    }
+
+    public void CloseToolsPanel()
+    {
+        OpenAllTools();
+        toolsPanel.blocksRaycasts = false;
+        toolsPanel.DOFade(0f, 0.5f);
+        openToolBtnCg.DOFade(1f, 0.35f);
+    }
+
+    public void SelectTool()
+    {
+        if (currIdx < 0 || currIdx >= toolItems.Count) return;
+
+        selectedTool = toolItems[currIdx];
+
+        CloseToolsPanel();
+
+        OnToolSelect(selectedTool);
+        OnToolSelected?.Invoke(selectedTool);
+    }
+
+    public void UnSelectTool()
+    {
+        OnToolSelect(null);
+        selectedTool = null;
+        currIdx = -1;
     }
 }
